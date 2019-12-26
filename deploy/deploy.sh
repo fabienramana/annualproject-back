eb0e6e83d362e025c250d2ff2921bcbe615ccc5e
spectre <fares.abderrazag@gmail.com>
2019-12-26 14:50:32 +0100

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,6 +12,7 @@ echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 # ** End of alternative approach
 
 # disable the host key checking.
+chmod a+x /deploy/disableHostKeyChecking
 ./deploy/disableHostKeyChecking.sh
 
 # we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
