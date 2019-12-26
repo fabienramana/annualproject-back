812259cf52a5dae50ba52f9d65010297802f7a46
spectre <fares.abderrazag@gmail.com>
2019-12-26 13:39:06 +0100

    test

--- deploy_deploy.sh
+++ deploy_deploy.sh
@@ -3,13 +3,15 @@
 # any future command that fails will exit the script
 set -e
 # Lets write the public key of our aws instance
-eval $(ssh-agent -s)
-echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 # ** Alternative approach
-# echo -e "$PRIVATE_KEY" > /root/.ssh/id_rsa
-# chmod 600 /root/.ssh/id_rsa
+eval $(ssh-agent -s)
+echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 # ** End of alternative approach
+
+echo -e "$PRIVATE_KEY" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+
 
 # disable the host key checking.
 ./deploy/disableHostKeyChecking.sh
